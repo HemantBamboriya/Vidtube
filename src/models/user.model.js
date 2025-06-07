df3c56d00a1a91e2170414d94caac7faaf38b6b6
Hemant Bamboriya <your-email@example.com>
2025-06-07 11:13:23 +0530

    cloudinary

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -59,7 +59,7 @@ const userSchema = new Schema(
   userSchema.pre("save", async function(next){
     if(!this.isModified("password")) return next();
 
-    this.password = bcrypt.hash(this.password,10)//(data to encrypt, how many rounds)
+    this.password =  await bcrypt.hash(this.password,10)//(data to encrypt, how many rounds)
     next()
   })
 
---
+++ src/models/video.model.js
@@ -0,0 +1,58 @@
+import mongoose,{Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+
+const videoSchema = new Schema
+(
+ {
+   videoFile:{
+    type:String,//cloudinary url
+    required:true,
+
+   },
+
+   thumbnail:{
+    type:String,
+    required:true,
+   },
+
+   tilte:{
+    type:String,
+    required:true,
+   },
+
+   description:{
+    type:String,
+    required:true,
+   },
+
+   duration:{
+   type:Number,//cloudary se dureation aata hai
+   required:true
+   },
+
+   views:{
+    type:Number,
+    default:0
+   },
+
+   isPublished:{
+    type:Boolean,
+    default:true,
+   },
+
+   owner:{
+    type:Schema.Types.ObjectId,
+    ref:"User"
+   }
+ },
+ {
+    timestamps:true
+ }
+)
+
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+
+export const Video = mongoose.model("Video",videoSchema)
