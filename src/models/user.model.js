3f8fa6fea3612e0f8e74536ad543ad42cdc38902
Hemant Bamboriya <your-email@example.com>
2025-06-12 20:16:10 +0530

    creating routes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -21,7 +21,7 @@ const userSchema = new Schema(
         trim:true,
        },
 
-       fullName:{
+       username:{
         type:String,
         required:true,
         trim:true,
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,6 +3,13 @@ import {loginUser,logoutUser,registerUser} from "../controllers/user.controller.
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { refreshAccessToken } from "../controllers/user.controller.js";
+import { changeCurrentPassword } from "../controllers/user.controller.js";
+import { getCurrentUser } from "../controllers/user.controller.js";
+import { updateAccountDetails } from "../controllers/user.controller.js";
+import { updateUserAvatar } from "../controllers/user.controller.js";
+import { updateUserCoverImage } from "../controllers/user.controller.js";
+import { getUserChannelProfile } from "../controllers/user.controller.js";
+import { getWatchHistory } from "../controllers/user.controller.js";
 const router = Router()
 
 router.route("/register").post(
@@ -23,6 +30,14 @@ router.route("/login").post(loginUser)
 //secure routes
 router.route("/logout").post(verifyJWT,logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+
+router.route("/change-password").post(verifyJWT,changeCurrentPassword)
+router.route("/current-user").post(verifyJWT,getCurrentUser)
+router.route("/update-account").patch(verifyJWT,updateAccountDetails)
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage)
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+router.route("/history").get(verifyJWT,getWatchHistory)
 
 
 export default router
